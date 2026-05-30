8c3be743500634d941301259c9d9a7ffe2d31320
Robin <robinchristy007@gmail.com>
2026-05-30 21:05:48 +0530

    Add /stream URL route

--- backend/chats/urls_detail.py
+++ backend/chats/urls_detail.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('<int:pk>', views.conversation_detail, name='conversation-detail'),
     path('<int:pk>/messages', views.send_message, name='message-send'),
+    path('<int:pk>/stream', views.stream_message, name='message-stream'),
 ]
